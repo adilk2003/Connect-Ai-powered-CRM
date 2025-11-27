825d0c47deae86421eddee28c26598c7d961b536
adilk2003 <149792652+adilk2003@users.noreply.github.com>
2025-11-27 13:09:18 +0530

    Add types for ImportMetaEnv and ImportMeta

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,3 +1,13 @@
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly API_KEY: string;
+  [key: string]: any;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
 declare namespace NodeJS {
   interface ProcessEnv {
     API_KEY: string;
